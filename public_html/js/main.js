b812bd51df4c722939bb7da41c592f204849e973
Robin <robinvanleeuwen1995@hotmail.com>
2014-12-09 20:42:36 +0100

    Minor update of search

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,3 +1,7 @@
+$( document ).ready(function() {
+	$( ".filterinput" ).focus();
+});
+
 (function ($) {
   jQuery.expr[':'].Contains = function(a,i,m){
       return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
@@ -8,37 +12,41 @@
     $(".filterinput")
       .keyup( function (e) {
         var code = e.keyCode || e.which;
- 
-
+		var searchtext = $(this).val();
+		if(code == 8 && !searchtext) { //backspace keycode			TODO: check if homepage // has to be fully empty
+            window.location.href = "index.html" //parent_page
+												
+          }
         //getting search value
-        var searchtext = $(this).val();
+        
         if(searchtext) {
           //finding If content matches with search keyword
           $matches = $(list).find('a:Contains(' + searchtext + ')').parent();
           //hiding non matching lists
           $('li', list).not($matches).slideUp();
           //showing matching lists
-          console.log($matches);
+		  //console.log($matches);
           //$matches.slideDown();
           if(code == 8) { //backspace keycode
-            console.log('test');
+          //console.log('test');
           }
           else{
           $pageA = $matches.html();
-          $pageUrl = $($pageA).attr('href');
+          $pageUrl = $($pageA).attr('href'); //url gotoPage
           $searchIndex = searchtext.length;
           console.log($searchIndex);
           if($matches.length == 1 && $searchIndex > "2"){
 	        $searchString = $matches[0].textContent.trim();
           	console.log($searchString);
           	$('.filterinput').val( $searchString );
+			gotoPage();		//TODO	has to load next input, only on enter gotoPage
           }
           }
           if(searchtext == "help"){
           	$(list).find("li").slideDown(200);
           }
           if(code == 13){
-            gotoPage();
+            gotoPage();		//goto page pas bij enter
            }
         }
         else {
@@ -53,7 +61,7 @@
   }
 
   $(function () {
-    live_search($("#contents"));
+    live_search($("#category"));
   });
 }(jQuery));
 
